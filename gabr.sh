4f4ff34ab7a56799b4200bffef9a47ab2dc75d66
Nico <nico@brinkkemper.nl>
2019-06-13 19:02:37 +0200

    test if -v  helps

--- gabr.sh
+++ gabr.sh
@@ -18,67 +18,67 @@
 # @exitcode >0 On failure
 #
 function gabr() {  # A function to run other functions 
-    if ! [ -v funcname ]; then
+    if ! [ -v 'funcname' ]; then
         local -a funcname=(${FUNCNAME[@]})
     fi
-    if ! [ -v filename ]; then
+    if ! [ -v 'filename' ]; then
         local filename
     fi
-    if ! [ -v pathJuggle ]; then
+    if ! [ -v 'pathJuggle' ]; then
         local pathJuggle
     fi
-    if ! [ -v exitcode ]; then
+    if ! [ -v 'exitcode' ]; then
         local exitcode
     fi
-    if ! [ -v args ]; then
+    if ! [ -v 'args' ]; then
         local -a args=()
     elif [ $# -eq 0 ]; then
         set -- ${args[@]:-}
     fi
-    if ! [ -v debug ]; then
+    if ! [ -v 'debug' ]; then
         local -a debug=()
     fi
-    if ! [ -v fn ]; then
+    if ! [ -v 'fn' ]; then
         local fn
     fi
-    if ! [ -v dir ]; then
+    if ! [ -v 'dir' ]; then
         local dir=.
     fi
-    if ! [ -v primaryFn ]; then
+    if ! [ -v 'primaryFn' ]; then
         local primaryFn=${1:-}
     fi
     if ! ((${#files[@]})); then
         local -A files=([$BASH_SOURCE]=$BASH_SOURCE)
     fi
-    if ! [ -v env ]; then
+    if ! [ -v 'env' ]; then
         local env=dev
     fi
-    if ! [ -v pwd ]; then
+    if ! [ -v 'pwd' ]; then
         local pwd="${PWD}"
     fi
     # Set from globals
-    if [ -v GABR_ROOT ]; then
+    if [ -v 'GABR_ROOT' ]; then
         root=$GABR_ROOT # Optionally set a fixed root through a global
     fi
-    if [ -v GABR_ENV ]; then
+    if [ -v 'GABR_ENV' ]; then
         env=$GABR_ENV
     fi
-    if [ -v GABR_DEFAULT ]; then
+    if [ -v 'GABR_DEFAULT' ]; then
         default=$GABR_DEFAULT # Optionally set a fixed namespace for 'usage' functionality
     fi
     if [ $env = prod ]; then
         set -euo pipefail # this will crash terminal on error
     fi
-    if [ $env = debug ] && ! [ -v debug ]; then
+    if [ $env = debug ] && ! [ -v 'debug' ]; then
         debug=(fn args dir filename)
     fi
-    if ! [ -v default ]; then
+    if ! [ -v 'default' ]; then
         local default=usage # By default a function called 'usage' prints a variable called 'usage' through variable expansion
     fi
-    if ! [ -v root ]; then
+    if ! [ -v 'root' ]; then
         local root=$PWD
     fi
-    if ! [ -v $default ]; then
+    if ! [ -v '$'default ]; then
         local $default="\
 ${FUNCNAME} [--file] [--derive] [file] function [arguments] -- A function to call other functions
     --file       A full path to a file
@@ -86,16 +86,16 @@ ${FUNCNAME} [--file] [--derive] [file] function [arguments] -- A function to cal
     1..N         Performs various checks to derive flags and optimize the API.
                  Flags are optional and not needed in most cases."
     fi
-    if ! [ -v stack ]; then
+    if ! [ -v 'stack' ]; then
         local stack=$(declare -F)
     fi
-    if ! [ -v wrapInfo ]; then
+    if ! [ -v 'wrapInfo' ]; then
         local wrapInfo="# "%s'\n'
     fi
-    if ! [ -v wrapErr ]; then
+    if ! [ -v 'wrapErr' ]; then
         local wrapErr=$'\033[0;91m'"Warning: "%s$'\033[0m\n' # printfn LightRed with newline -- e.g. printfn ${wrapErr} "something went wrong"
     fi
-    if ! [ -v error ]; then
+    if ! [ -v 'error' ]; then
         local -a error=()
     fi
 ( # @enter subshell
@@ -117,11 +117,11 @@ $default(){
     local prevFn=''
     while [ $# -ne 0 ];
     do
-        if [ -v fn ]; then
+        if [ -v 'fn' ]; then
             prevFn=$fn
         fi
         fn=${1:-$default}; shift; args=(${@});
-        if [ -v debug ]; then
+        if [ -v 'debug' ]; then
             echo "# -----------" >&2
         fi
         if [ ${fn::2} = '-' ]; then
@@ -129,7 +129,7 @@ $default(){
         elif [ ${fn::2} = '--' ]; then
             if [ ${fn^^} = '--FILE' ] || [ ${fn^^} = '--DERIVE' ]; then
                 
-                if ! [ -v args  ]; then
+                if ! [ -v 'args'  ]; then
                     error+=("Can not find a file without arguments")
                     printf "$wrapErr" "${error[*]}" 1>&2
                     return 1;
@@ -137,7 +137,7 @@ $default(){
                     error+=("File not found: ${args}")
                     printf "$wrapErr" "${error[*]}" 1>&2
                     return 1;
-                elif [ -v files[$args] ]; then
+                elif [ -v 'files'[$args] ]; then
                     error+=("File already imported: ${args}")
                     printf "$wrapErr" "${error[*]}" 1>&2
                     return 1
@@ -149,8 +149,8 @@ $default(){
                     files+=([$fn]=$fn)
                     source $fn
                     if [ $(type -t ${filename}) = function ]; then
-                        if [ ${prevFn^^} = '--DERIVE' ] || ! [ -v args ] || [ ${args::1} = '-' ]; then
-                            if [ -v debug ]; then
+                        if [ ${prevFn^^} = '--DERIVE' ] || ! [ -v 'args' ] || [ ${args::1} = '-' ]; then
+                            if [ -v 'debug' ]; then
                                 printf $wrapInfo "${fn} is derived" >&2
                             fi
                             set -- ${filename} ${args[@]:-}
@@ -159,7 +159,7 @@ $default(){
                 fi
             fi
         elif [ "$(type -t ${fn:-})" = function ]; then
-            if [ -v debug ]; then
+            if [ -v 'debug' ]; then
                 printf "$wrapInfo" "Calling ${fn}" >&2
             fi
             cd $dir
@@ -184,7 +184,7 @@ $default(){
         elif ! [ $PWD = $root ]; then # allow the same as above, but in root directory
             cd $root
             set -- $fn ${args[@]:-}   
-            if [ -v debug ]; then
+            if [ -v 'debug' ]; then
                 printf $wrapInfo "Nothing found, switched to root as last resort" >&2
             fi         
         else
@@ -192,11 +192,11 @@ $default(){
             printf "$wrapErr" "${error[*]}" 1>&2
             return 1
         fi
-        if [ -v debug ]; then
+        if [ -v 'debug' ]; then
             for val in ${debug[@]}
             do
                 local valArr=${val}[@]
-                if [ -v $val ] || [ -n ${!valArr+set} ]; then
+                if [ -v '$'val ] || [ -n ${!valArr+set} ]; then
                     echo "# $(declare -p $val 2>/dev/null | cut -d' ' -f 3-)" >&2
                 fi
             done
