77a9802f5dfaa1e121a0f031df967b09a4f6f074
Nico <nico@brinkkemper.nl>
2019-06-13 20:22:35 +0200

    try bash 3

--- gabr.linux.sh
+++ gabr.linux.sh
@@ -17,133 +17,146 @@
 # @exitcode 0  If successfull
 # @exitcode >0 On failure
 #
+if ((BASH_VERSINFO < 4))
+then
+  echo "Please update your bash version to 4.4" >&2
+  echo "Using legacy gabr" >&2
+  source ./${BASH_SOURCE##*\/}/legacy-gabr.sh
+else
 function gabr() {  # A function to run other functions 
-    if [ -z "${funcname+set}" ]; then
+    if ! [ -v funcname ]; then
         local -a funcname=(${FUNCNAME[@]})
     fi
-    if [ -z "${filename+set}" ]; then
+    if ! [ -v filename ]; then
         local filename
     fi
-    if [ -z "${pathJuggle+set}" ]; then
+    if ! [ -v pathJuggle ]; then
         local pathJuggle
     fi
-    if [ -z "${exitcode+set}" ]; then
+    if ! [ -v exitcode ]; then
         local exitcode
     fi
-    if [ -z "${args+set}" ]; then
+    if ! [ -v args ]; then
         local -a args=()
     elif [ $# -eq 0 ]; then
         set -- ${args[@]:-}
     fi
-    if [ -z "${debug+set}" ]; then
+    if ! [ -v debug ]; then
         local -a debug=()
     fi
-    if [ -z "${fn+set}" ]; then
+    if ! [ -v fn ]; then
         local fn
     fi
-    if [ -z "${dir+set}" ]; then
+    if ! [ -v dir ]; then
         local dir=.
     fi
-    if [ -z "${primaryFn+set}" ]; then
+    if ! [ -v primaryFn ]; then
         local primaryFn=${1:-}
     fi
-    if [ -z "${env+set}" ]; then
+    if ! ((${#files[@]})); then
+        local -A files=([$BASH_SOURCE]=$BASH_SOURCE)
+    fi
+    if ! [ -v env ]; then
         local env=dev
     fi
-    if [ -z "${pwd+set}" ]; then
+    if ! [ -v pwd ]; then
         local pwd="${PWD}"
     fi
-    if [ "$env" = 'debug' ] && [ -z "${debug+set}" ]; then
+    # Set from globals
+    if [ -v GABR_ROOT ]; then
+        root=$GABR_ROOT # Optionally set a fixed root through a global
+    fi
+    if [ -v GABR_ENV ]; then
+        env=$GABR_ENV
+    fi
+    if [ -v GABR_DEFAULT ]; then
+        default=$GABR_DEFAULT # Optionally set a fixed namespace for 'usage' functionality
+    fi
+    if [ $env = prod ]; then
+        set -euo pipefail # this will crash terminal on error
+    fi
+    if [ $env = debug ] && ! [ -v debug ]; then
         debug=(fn args dir filename)
     fi
-    if [ -z "${default+set}" ]; then
-        local default=usage 
+    if ! [ -v default ]; then
+        local default=usage # By default a function called 'usage' prints a variable called 'usage' through variable expansion
     fi
-    if [ -z "${root+set}" ]; then
+    if ! [ -v root ]; then
         local root=$PWD
     fi
-    if [ -z "${usage+set}" ]; then
-        local usage="\
+    if ! [ -v $default ]; then
+        local $default="\
 ${FUNCNAME} [--file] [--derive] [file] function [arguments] -- A function to call other functions
     --file       A full path to a file
     --derive     A filename without extension
     1..N         Performs various checks to derive flags and optimize the API.
                  Flags are optional and not needed in most cases."
     fi
-    if [ -z "${stack+set}" ]; then
+    if ! [ -v stack ]; then
         local stack=$(declare -F)
     fi
-    if [ -z "${wrapInfo+set}" ]; then
+    if ! [ -v wrapInfo ]; then
         local wrapInfo="# "%s'\n'
     fi
-    if [ -z "${wrapErr+set}" ]; then
+    if ! [ -v wrapErr ]; then
         local wrapErr=$'\033[0;91m'"Warning: "%s$'\033[0m\n' # printfn LightRed with newline -- e.g. printfn ${wrapErr} "something went wrong"
     fi
-    if [ -z "${error+set}" ]; then
+    if ! [ -v error ]; then
         local -a error=()
-    fi
-    # Set from globals
-    if ! [ -z "${GABR_ROOT+set}" ]; then
-        root=${GABR_ROOT:-${PWD}} # Optionally set a fixed root through a global
-    fi
-    if ! [ -z "${GABR_ENV+set}" ]; then
-        env=${GABR_ENV:-dev}
-    fi
-    if ! [ -z "${GABR_DEFAULT+set}" ]; then
-        default=${GABR_DEFAULT:-usage} # Optionally set a fixed namespace for 'usage' functionality
-    fi
-    # Set prod mode
-    if [ "$env" = 'prod' ]; then
-        set -euo pipefail # this will crash terminal on error
     fi
 ( # @enter subshell
     local IFS=$'\n\t'
-    if [ "$env" = 'dev' ] || [ "$env" = 'debug' ]; then
+    if [ $env = dev ] || [ $env = debug ]; then
         set -Euo pipefail
     fi
     trap 'exitcode=$?; cd $pwd; return $exitcode' ERR SIGINT
-    if ! [ "$(type -t $default)" = 'function' ]; then
+    if ! [ "$(type -t $default)" = function ]; then
         eval "\
-${default:-usage}(){
-    echo \"\${usage}\" >&2
+$default(){
+    echo \"\${!default}\" >&2
 }";
     fi
-    if [ "$#" -eq 0 ]; then
+    if [ $# -eq 0 ]; then
         $default
         return;
     fi
     local prevFn=''
-    while [ "$#" -ne 0 ];
+    while [ $# -ne 0 ];
     do
-        if [ -v 'fn' ]; then
+        if [ -v fn ]; then
             prevFn=$fn
         fi
         fn=${1:-$default}; shift; args=(${@});
-        if [ -v 'debug' ]; then
+        if [ -v debug ]; then
             echo "# -----------" >&2
         fi
-        if [ "${fn::2}" = '-' ]; then
+        if [ ${fn::2} = '-' ]; then
             break
-        elif [ "${fn::2}" = '--' ]; then
-            if [ "${fn^^}" = '--FILE' ] || [ "${fn^^}" = '--DERIVE' ]; then
+        elif [ ${fn::2} = '--' ]; then
+            if [ ${fn^^} = '--FILE' ] || [ ${fn^^} = '--DERIVE' ]; then
                 
-                if [ -z "${args+set}"  ]; then
+                if ! [ -v args  ]; then
                     error+=("Can not find a file without arguments")
                     printf "$wrapErr" "${error[*]}" 1>&2
                     return 1;
-                elif  ! [ -r "${args}" ]; then
+                elif  ! [ -r ${args} ]; then
                     error+=("File not found: ${args}")
                     printf "$wrapErr" "${error[*]}" 1>&2
                     return 1;
+                elif [ -v files[$args] ]; then
+                    error+=("File already imported: ${args}")
+                    printf "$wrapErr" "${error[*]}" 1>&2
+                    return 1
                 else
                     prevFn=$fn
                     fn=$1; shift; args=(${@});
                     pathJuggle=${fn##*/}
                     filename=${pathJuggle%%.*}
+                    files+=([$fn]=$fn)
                     source $fn
-                    if [ "$(type -t ${filename})" = 'function' ]; then
-                        if [ ${prevFn^^} = '--DERIVE' ] || [ -z "${args+set}" ] || [ ${args::1} = '-' ]; then
-                            if [ -v 'debug' ]; then
+                    if [ $(type -t ${filename}) = function ]; then
+                        if [ ${prevFn^^} = '--DERIVE' ] || ! [ -v args ] || [ ${args::1} = '-' ]; then
+                            if [ -v debug ]; then
                                 printf $wrapInfo "${fn} is derived" >&2
                             fi
                             set -- ${filename} ${args[@]:-}
@@ -151,8 +164,8 @@ ${default:-usage}(){
                     fi
                 fi
             fi
-        elif [ "$(type -t ${fn:-})" = 'function' ]; then
-            if [ -v 'debug' ]; then
+        elif [ "$(type -t ${fn:-})" = function ]; then
+            if [ -v debug ]; then
                 printf "$wrapInfo" "Calling ${fn}" >&2
             fi
             cd $dir
@@ -160,24 +173,24 @@ ${default:-usage}(){
             $fn ${args[@]:-};
             cd $pwd
             return $?
-        elif [ -f "$fn" ]; then # allow file
+        elif [ -f $fn ]; then # allow file
             set -- --file $fn ${args[@]:-}                             
-        elif [ -f "${dir}/$fn" ]; then # allow files in dir
+        elif [ -f ${dir}/$fn ]; then # allow files in dir
             set -- --file ${dir}/$fn ${args[@]:-}
-        elif [ -f "${dir}/$fn.sh" ]; then # allow files omitting .sh
+        elif [ -f ${dir}/$fn.sh ]; then # allow files omitting .sh
             set -- --derive ${dir}/$fn.sh ${args[@]:-}
-        elif [ -f  "${dir}/${fn}/$fn.sh" ]; then # allow dir same as file
+        elif [ -f  ${dir}/${fn}/$fn.sh ]; then # allow dir same as file
             set -- --derive ${dir}/${fn}/$fn.sh ${args[@]:-}
             dir+=/$fn 
-        elif [ -d "$fn" ]; then # allow directory
+        elif [ -d $fn ]; then # allow directory
             dir+=/$fn
-        elif ! [ "$dir" = '.' ]; then # allow the same as above, but in current directory
+        elif ! [ $dir = . ]; then # allow the same as above, but in current directory
             dir=.
             set -- $fn ${args[@]:-}
-        elif ! [ "$PWD" = "$root" ]; then # allow the same as above, but in root directory
+        elif ! [ $PWD = $root ]; then # allow the same as above, but in root directory
             cd $root
             set -- $fn ${args[@]:-}   
-            if [ -v 'debug' ]; then
+            if [ -v debug ]; then
                 printf $wrapInfo "Nothing found, switched to root as last resort" >&2
             fi         
         else
@@ -185,11 +198,11 @@ ${default:-usage}(){
             printf "$wrapErr" "${error[*]}" 1>&2
             return 1
         fi
-        if [ -v 'debug' ]; then
+        if [ -v debug ]; then
             for val in ${debug[@]}
             do
                 local valArr=${val}[@]
-                if [ -v '$'val ] || [ -n ${!valArr+set} ]; then
+                if [ -v $val ] || [ -n ${!valArr+set} ]; then
                     echo "# $(declare -p $val 2>/dev/null | cut -d' ' -f 3-)" >&2
                 fi
             done
@@ -199,7 +212,7 @@ ${default:-usage}(){
 # @close subshell
 )
 }
-
+fi
 if [ "$0" = "$BASH_SOURCE" ]; then
     declare IFS=$'\n\t'
     gabr ${*}
